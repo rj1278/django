d80e91bb3aaf74b8151df03f1aeaffb5b89a96c3
rj1278 <90405007+rj1278@users.noreply.github.com>
2025-04-21 20:31:59 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4n!$3mx148c$(_9$niu7==fx77dg7tvf5v2sbmfezxpes@smm(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
